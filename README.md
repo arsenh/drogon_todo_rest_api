13761f261197211fb51e485b3f84d00cbbc381a8
Arsen Hovhannisyan <arsen1994t@gmail.com>
2025-02-27 22:58:42 +0400

    Add Drogon Framework

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # drogon_todo_rest_api
 Todo REST API on Drogon Framework
+
+Compile commands:
+In application directory.
+
+$ conan install . --output-folder=build --build=missing
+$ cd build
+$ cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "server/include/server.hpp"
 
+#include <fmt/core.h>
+#include <drogon/drogon.h>
+
+using namespace drogon;
+
 int main() {
-    std::cout << "Main program\n";
-    start();
+    app().setLogPath("./")
+         .setLogLevel(trantor::Logger::kWarn)
+         .addListener("0.0.0.0", 8080)
+         .setThreadNum(16)
+         .run();
     return 0;
 }
 
