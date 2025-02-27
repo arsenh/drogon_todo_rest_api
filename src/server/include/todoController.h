57eaf2ce451768d9ccd9ffe8589698b8e80898b1
Arsen Hovhannisyan <arsen1994t@gmail.com>
2025-02-28 00:43:41 +0400

    Add prefix for Todo controller

--- src/server/include/todoController.h
+++ src/server/include/todoController.h
@@ -2,9 +2,12 @@
 
 #include <drogon/HttpController.h>
 
+#include <string>
+
 using namespace drogon;
 class TodoController : public HttpController<TodoController, false>
 {
+    static constexpr  std::string_view prefix = "/todo";
 public:
     TodoController()
     {
@@ -12,8 +15,11 @@ public:
     }
 
     METHOD_LIST_BEGIN
-        ADD_METHOD_TO(TodoController::test_api, "/test", Get);
+        ADD_METHOD_TO(TodoController::test_api, getPatternPrefix("/test"), Get);
     METHOD_LIST_END
 
     void test_api(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback);
+
+private:
+    static std::string getPatternPrefix(std::string pattern);
 };
--- src/server/src/todoController.cpp
+++ src/server/src/todoController.cpp
@@ -1,13 +1,22 @@
 #include "todoController.h"
 
+#include <format>
 
 void TodoController::test_api(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
 {
-    LOG_DEBUG << "test_api called";
+    static int counter = 0;
+    LOG_DEBUG << "test_api called" << counter;
 
     Json::Value data;
     data["result"] = "ok";
-    data["message"] = "Hello World!";
+    data["message"] = std::format("Hello World! {}", counter);
     auto resp = HttpResponse::newHttpJsonResponse(data);
+    ++counter;
     callback(resp);
 }
+
+std::string TodoController::getPatternPrefix(std::string pattern)
+{
+    return std::format("{}{}", prefix, pattern);
+}
+
